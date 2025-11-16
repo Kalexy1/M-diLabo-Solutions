872818a9f53d31bdaa01d6ecf6cc2e56326ebebc
Alexy Kieraga <43311202+Kalexy1@users.noreply.github.com>
2025-11-16 09:25:47 +0100

    Update dockercompose

--- projet-root/mysql-init/01-init.sql
+++ projet-root/mysql-init/01-init.sql
@@ -1,2 +1,3 @@
-CREATE DATABASE IF NOT EXISTS authdb CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-CREATE DATABASE IF NOT EXISTS patientdb CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+-- Création des bases attendues par les services
+CREATE DATABASE IF NOT EXISTS medilabo_gateway CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS patientdb        CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
